38851d165e8222e50258724bef60718131f7cb63
toofun666 <tufan.kaynak@framras.com>
2019-12-29 13:01:53 +0300

    efatura parameters

---
+++ trebelge/trebelge/doctype/tr_gib_efatura_settings/tr_gib_efatura_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, Framras AS-Izmir and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('TR GIB eFatura Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
