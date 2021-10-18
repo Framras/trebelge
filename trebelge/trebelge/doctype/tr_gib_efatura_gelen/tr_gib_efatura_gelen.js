65d4672cc3e8b729c08156d7de73b01bd20e22f7
toofun666 <tufan@kaynak.gen.tr>
2021-10-18 18:52:57 +0300

    refactor: can update and run long queries

---
+++ trebelge/trebelge/doctype/tr_gib_efatura_gelen/tr_gib_efatura_gelen.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Framras AS-Izmir and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('TR GIB eFatura Gelen', {
+	// refresh: function(frm) {
+
+	// }
+});
