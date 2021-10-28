e61b0dbcca3f80d86f02e35ee2e637ebde6bf69e
toofun666 <tufan@kaynak.gen.tr>
2021-10-28 18:01:03 +0300

    Adding complete UBL object types

--- trebelge/api.py
+++ trebelge/api.py
@@ -44,10 +44,18 @@ def get_ebelge_users():
         frappe.read_file(frappe.get_site_path("private", "files", "KullaniciListesiXml", "newUserPkList.xml")))
     return parser.close()
 
+
 @frappe.whitelist()
 def check_all_ebelge_files():
-    for ebelgeFile in frappe.get_all(party_type, filters={"tax_id": ["not in", None], "disabled": 0},
-                                     fields={"name", "tax_id", "is_efatura_user", "is_eirsaliye_user"}):
+    for xmlFile in frappe.get_all('File', filters={"file_name": ["like", "%.xml"], "is_folder": 0},
+                                  fields={"name", "content_hash"}):
+        # check if record exists by filters
+        if not frappe.db.exists({"doctype": "TR GIB eFatura Gelen",
+                                 "file": xmlFile.get('name'),
+                                 "content_hash": xmlFile.get('content_hash')}):
+            read_ebelge_file()
+    return frappe.utils.nowdate()
+
 
 def read_ebelge_file():
     filename = '/home/tufankaynak/bench/sites/trgibebelgedev/private/files/13D2021000002726.xml'
