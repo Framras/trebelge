776308b697f2313da0fc3c3c581af7a4962a5272
toofun666 <tufan@kaynak.gen.tr>
2021-10-30 00:26:52 +0300

    Adding complete UBL object types

---
+++ trebelge/trebelge/doctype/tr_gib_ebelge_switchboard/tr_gib_ebelge_switchboard.js
@@ -0,0 +1,18 @@
+// Copyright (c) 2021, Framras AS-Izmir and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('TR GIB eBelge Switchboard', {
+	// refresh: function(frm) {
+	check_xml_files: function(frm){
+        frappe.call({
+            method: "trebelge.api.check_all_xml_files",
+            args:{
+            },
+            callback: function(r){
+                frm.set_value("last_run", r.message);
+                frm.save();
+            }
+        })
+	}
+	// }
+});
--- trebelge/trebelge/doctype/tr_gib_efatura_gelen/tr_gib_efatura_gelen.js
+++ trebelge/trebelge/doctype/tr_gib_efatura_gelen/tr_gib_efatura_gelen.js
@@ -3,16 +3,6 @@
 
 frappe.ui.form.on('TR GIB eFatura Gelen', {
 	// refresh: function(frm) {
-	check_xml_files: function(frm){
-        frappe.call({
-            method: "trebelge.api.check_all_xml_files",
-            args:{
-            },
-            callback: function(r){
-                frm.set_value("last_run", r.message);
-                frm.save();
-            }
-        })
-	}
+
 	// }
 });
