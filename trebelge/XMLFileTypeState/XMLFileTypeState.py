425b251148a5db6243d36f9def1109b3b91c8412
toofun666 <tufan@kaynak.gen.tr>
2021-11-01 23:37:24 +0300

    Adding complete UBL object types

---
+++ trebelge/XMLFileTypeState/XMLFileTypeState.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+
+class XMLFileTypeState(ABC):
+    """
+    The base State class declares methods that all Concrete State should
+    implement and also provides a backreference to the Context object,
+    associated with the State. This backreference can be used by States to
+    transition the Context to another State.
+    """
+
+    @property
+    def context(self) -> Context:
+        return self._context
+
+    @context.setter
+    def context(self, context: Context) -> None:
+        self._context = context
+
+    @abstractmethod
+    def handle1(self) -> None:
+        pass
+
+    @abstractmethod
+    def handle2(self) -> None:
+        pass
