422252039b40d5037ca5509c2384f48ea5b3a610
toofun666 <tufan@kaynak.gen.tr>
2021-11-01 23:40:37 +0300

    Adding complete UBL object types

---
+++ trebelge/XMLFileTypeState/XMLFileTypeContext.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+
+class XMLFileTypeContext:
+    """
+    The Context defines the interface of interest to clients. It also maintains
+    a reference to an instance of a State subclass, which represents the current
+    state of the Context.
+    """
+
+    _state = None
+    """
+    A reference to the current state of the Context.
+    """
+
+    def __init__(self, state: XMLFileTypeState) -> None:
+        self.transition_to(state)
+
+    def transition_to(self, state: State):
+        """
+        The Context allows changing the State object at runtime.
+        """
+
+        print(f"Context: Transition to {type(state).__name__}")
+        self._state = state
+        self._state.context = self
+
+    """
+    The Context delegates part of its behavior to the current State object.
+    """
+
+    def request1(self):
+        self._state.handle1()
+
+    def request2(self):
+        self._state.handle2()
