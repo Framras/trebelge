3994216be22e2a29300def195301b7f75450b1cb
toofun666 <tufan@kaynak.gen.tr>
2021-11-01 22:39:52 +0300

    Adding complete UBL object types

--- trebelge/XMLFileTypeCoR/DespatchAdviceHandler.py
+++ trebelge/XMLFileTypeCoR/DespatchAdviceHandler.py
@@ -8,11 +8,11 @@ class DespatchAdviceHandler(AbstractXMLFileTypeHandler):
     This Handler has no successor.
     CoR Handler method for checking if the FileType is DespatchAdvice.
     """
+    despatchAdvice_namespace = frappe.db.get_single_value('TR GIB eBelge Switchboard',
+                                                          'despatch_advice_namespace_specification')
 
     def handleRequest(self, filepath):
-        if ET.parse(filepath).getroot().tag == frappe.db.get_single_value('TR GIB eBelge Switchboard',
-                                                                          'despatch_advice_namespace_specification'
-                                                                          ) + 'DespatchAdvice':
+        if ET.parse(filepath).getroot().tag == self.despatchAdvice_namespace + 'DespatchAdvice':
             # TODO: Implement DespatchAdvice processing
             pass
         else:
