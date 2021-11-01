cbd3d8f79776c6b7a0d0db6e7554e82a34d2332c
toofun666 <tufan@kaynak.gen.tr>
2021-11-01 21:09:56 +0300

    Adding complete UBL object types

--- trebelge/XMLFileProcessManager.py
+++ trebelge/XMLFileProcessManager.py
@@ -1,1 +1,13 @@
-class XMLFileProcessManager:
+from trebelge.XMLFileTypeCoR import IXMLFileTypeHandler
+import XMLFileProcessStateManager
+
+
+class XMLFileProcessManager():
+    self.xmlpsm = XMLFileProcessStateManager
+    self.hInvoice = IXMLFileTypeHandler
+
+    def __init__(self, filepath):
+        self.xmlpsm = XMLFileProcessStateManager()
+
+        # create ConcreteHandler instances
+        self.hInvoice =
--- trebelge/XMLFileProcessStateManager.py
+++ trebelge/XMLFileProcessStateManager.py
@@ -1,2 +1,1 @@
 class XMLFileProcessStateManager:
-    xmlpsm = XMLFileProcessStateManager()
