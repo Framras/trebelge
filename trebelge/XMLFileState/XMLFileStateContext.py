e6258390f517cbefd140b327715a33f5b4daa6d1
toofun666 <tufan@kaynak.gen.tr>
2021-11-12 14:44:14 +0300

    Refactoring

--- trebelge/XMLFileState/XMLFileStateContext.py
+++ trebelge/XMLFileState/XMLFileStateContext.py
@@ -19,10 +19,6 @@ class XMLFileStateContext:
     _cXMLFileTypeStateContext = XMLFileTypeStateContext()
 
     def __init__(self, file_path: str):
-        # initiate Context of State pattern for FileType
-        self._cXMLFileTypeStateContext.set_file_path(file_path)
-        # handle file by CoR to determine State
-        self._hXMLFileHandler.handle_xml_file(self._cXMLFileTypeStateContext)
         # check on State if file is previously processed and recorded
         self._cXMLFileTypeStateContext.find_record_status()
         # TODO: Take it from here (The current State in StateContext is the next step)
