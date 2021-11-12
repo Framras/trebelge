10599022a02e1bdd9db5e17eaefbe9529699b2fc
toofun666 <tufan@kaynak.gen.tr>
2021-11-12 21:23:59 +0300

    Refactoring

---
+++ trebelge/XMLFileState/AllowanceCharge.py
@@ -0,0 +1,26 @@
+# from __future__ import annotations
+
+from trebelge.XMLFileState.AbstractXMLFileState import AbstractXMLFileState
+
+
+class AllowanceCharge(AbstractXMLFileState):
+    """
+    State methods
+    Backreference to the Context object, associated with the State.
+    """
+    _frappeDoctype: str = 'TR GIB eFatura Gelen'
+    _mapping = dict()
+
+    def find_ebelge_status(self):
+        pass
+
+    def define_mappings(self):
+        # _mapping[tag] = (namespace, frappe_field, cardinality, start_event, has_attribs, end_event)
+        self._mapping['ChargeIndicator'] = ('cbc', '', 'Zorunlu (1)', False, False, True)
+        self._mapping['AllowanceChargeReason'] = ('cbc', '', 'Seçimli (0...1)', False, False, True)
+        self._mapping['MultiplierFactorNumeric'] = ('cbc', '', 'Seçimli (0...1)', False, False, True)
+        self._mapping['SequenceNumeric'] = ('cbc', '', 'Seçimli (0...1)', False, False, True)
+        self._mapping['Amount'] = ('cbc', '', 'Zorunlu (1)', False, False, True)
+        self._mapping['BaseAmount'] = ('cbc', '', 'Seçimli (0...1)', False, False, True)
+        self._mapping['PerUnitAmount'] = ('cbc', '', 'Seçimli (0...1)', False, False, True)
+        self._mapping['AllowanceCharge'] = ('cac', 'AllowanceCharge', '', False, False, True)
