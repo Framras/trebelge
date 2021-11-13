7227daf410a9ef6251d498b9d4d14414d4cb325a
toofun666 <tufan@kaynak.gen.tr>
2021-11-13 10:31:09 +0300

    Refactoring

---
+++ trebelge/XMLFileState/Branch.py
@@ -0,0 +1,22 @@
+# from __future__ import annotations
+
+from trebelge.XMLFileState.AbstractXMLFileState import AbstractXMLFileState
+
+
+class Branch(AbstractXMLFileState):
+    """
+    State methods
+    Backreference to the Context object, associated with the State.
+    """
+    _frappeDoctype: str = 'TR GIB eFatura Gelen'
+    _mapping = dict()
+    _elementTag: str = 'FinancialInstitutionBranch'
+
+    def find_ebelge_status(self):
+        pass
+
+    def define_mappings(self):
+        # _mapping[tag] = (namespace, frappe_field, cardinality, start_event, has_attribs, end_event)
+        self._mapping['Name'] = ('cbc', '', 'Seçimli(0..1)', False, False, True)
+        self._mapping['FinancialInstitution'] = ('cac', 'FinancialInstitution', '', True, False, False)
+        self._mapping['Branch'] = ('cac', 'Branch', '', False, False, True)
