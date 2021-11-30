125d40722dcb64f206516776ce259e33f584de4f
toofun666 <tufan@kaynak.gen.tr>
2021-11-30 08:48:38 +0300

    Refactoring

--- trebelge/TRUBLCommonElementsStrategy/TRUBLPartyIdentification.py
+++ trebelge/TRUBLCommonElementsStrategy/TRUBLPartyIdentification.py
@@ -1,15 +1,26 @@
 from xml.etree.ElementTree import Element
 
+import frappe
 from trebelge.TRUBLCommonElementsStrategy.TRUBLCommonElement import TRUBLCommonElement
 
 
 class TRUBLPartyIdentification(TRUBLCommonElement):
+    _frappeDoctype: str = 'UBL TR Party Identification'
+    _frappeDoc = None
+
     def process_element(self, element: Element, cbcnamespace: str, cacnamespace: str) -> dict:
         # ['ID'] = ('cbc', 'id', 'Zorunlu (1)')
         # ['schemeID'] = ('', 'schemeid', 'Zorunlu (1)')
         partyidentification_ = element.find(cbcnamespace + 'ID')
-        partyidentification: dict = {'id': partyidentification_.text,
+        partyidentification: dict = {'doctype': self._frappeDoctype,
+                                     'id': partyidentification_.text,
                                      'schemeid': partyidentification_.attrib.get('schemeID')
                                      }
+        # TODO convert to table multiselect
+        if frappe.db.exists(partyidentification):
+            self._frappeDoc = frappe.get_doc(partyidentification)
+        else:
+            self._frappeDoc = frappe.get_doc(partyidentification)
+            self._frappeDoc.insert()
 
-        return partyidentification
+        return self._frappeDoc.get_value('name')
--- trebelge/trebelge/doctype/ubl_tr_communication/ubl_tr_communication.py
+++ trebelge/trebelge/doctype/ubl_tr_communication/ubl_tr_communication.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class TRUBLCommunication(Document):
+class UBLTRCommunication(Document):
     pass
