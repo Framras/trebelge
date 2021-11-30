2523498898d42ca2fe08eaa77eedbe6a191f5561
toofun666 <tufan@kaynak.gen.tr>
2021-11-30 23:43:01 +0300

    Refactoring

--- trebelge/trebelge/doctype/tr_ubl_billingreference/tr_ubl_billingreference.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021, Framras AS-Izmir and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('TR UBL BillingReference', {
-	// refresh: function(frm) {
-
-	// }
-});
--- trebelge/trebelge/doctype/tr_ubl_period_durationmeasure_unitcode/tr_ubl_period_durationmeasure_unitcode.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021, Framras AS-Izmir and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('TR UBL Period DurationMeasure unitCode', {
-	// refresh: function(frm) {
-
-	// }
-});
