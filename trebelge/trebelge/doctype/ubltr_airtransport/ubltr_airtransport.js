aedeade0d4a86aa352327705bc63ba672fd26a01
toofun666 <tufan@kaynak.gen.tr>
2021-12-04 22:39:10 +0300

    Refactoring

--- trebelge/trebelge/doctype/ubltr_airtransport/ubltr_airtransport.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021, Framras AS-Izmir and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('UBLTR AirTransport', {
-	// refresh: function(frm) {
-
-	// }
-});
