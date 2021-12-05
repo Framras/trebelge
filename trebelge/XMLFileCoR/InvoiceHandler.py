a8fb61b1d99ab24cc60333b756254991c171fe5f
toofun666 <tufan@kaynak.gen.tr>
2021-12-05 19:46:00 +0300

    Refactoring

--- trebelge/XMLFileCoR/InvoiceHandler.py
+++ trebelge/XMLFileCoR/InvoiceHandler.py
@@ -20,7 +20,7 @@ class InvoiceHandler(AbstractXMLFileHandler):
         for namespace in frappe.get_all(
                 self._eBelgeSettingsDoctype, filters={"disabled": 0, "ebelge_type": self._eBelgeTag},
                 fields={"namespace_specification"}):
-            if ET.parse(file_path).getroot().tag == namespace + self._eBelgeTag:
+            if ET.parse(file_path).getroot().tag == namespace.get('namespace_specification') + self._eBelgeTag:
                 director: TRUBLDirector = TRUBLDirector()
                 director.set_file_path(file_path)
                 director.builder = TRUBLInvoiceBuilder(director.get_uuid())
