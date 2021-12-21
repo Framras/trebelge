df751e4ed2939c835cc69e964d045fec2da84b01
toofun666 <tufan@kaynak.gen.tr>
2021-12-21 16:52:16 +0300

    Refactoring

--- trebelge/TRUBLCommonElementsStrategy/TRUBLTaxSubtotal.py
+++ trebelge/TRUBLCommonElementsStrategy/TRUBLTaxSubtotal.py
@@ -1,63 +1,54 @@
 from xml.etree.ElementTree import Element
 
+from frappe.model.document import Document
 from trebelge.TRUBLCommonElementsStrategy.TRUBLCommonElement import TRUBLCommonElement
 from trebelge.TRUBLCommonElementsStrategy.TRUBLCommonElementContext import TRUBLCommonElementContext
 from trebelge.TRUBLCommonElementsStrategy.TRUBLTaxCategory import TRUBLTaxCategory
 
 
 class TRUBLTaxSubtotal(TRUBLCommonElement):
+    _frappeDoctype: str = 'UBL TR TaxSubtotal'
     _strategyContext: TRUBLCommonElementContext = TRUBLCommonElementContext()
 
     def process_element(self, element: Element, cbcnamespace: str, cacnamespace: str) -> Document:
-        """
-        ['TaxableAmount'] = ('cbc', 'taxableamount', 'Seçimli (0...1)')
-        ['currencyID'] = ('', 'taxableamount_currencyid', 'Zorunlu(1)')
-        ['TaxAmount'] = ('cbc', 'taxamount', 'Zorunlu(1)')
-        ['currencyID'] = ('', 'taxamount_currencyid', 'Zorunlu(1)')
-        ['CalculationSequenceNumeric'] = ('cbc', 'calculationsequencenumeric', 'Seçimli (0...1)')
-        ['TransactionCurrencyTaxAmount'] = ('cbc', 'transactioncurrencytaxamount', 'Seçimli (0...1)')
-        ['currencyID'] = ('', 'transactioncurrencytaxamount_currencyid', 'Zorunlu(1)')
-        ['Percent'] = ('cbc', 'percent', 'Seçimli (0...1)')
-        ['BaseUnitMeasure'] = ('cbc', 'baseunitmeasure', 'Seçimli (0...1)')
-        ['unitCode'] = ('', 'baseunitmeasure_unitcode', 'Zorunlu(1)')
-        ['PerUnitAmount'] = ('cbc', 'perunitamount', 'Seçimli (0...1)')
-        ['currencyID'] = ('', 'perunitamount_currencyid', 'Zorunlu(1)')
-        ['TaxCategory'] = ('cac', 'taxcategory', 'Zorunlu(1)')
-        """
+        # ['TaxAmount'] = ('cbc', 'taxamount', 'Zorunlu(1)')
         taxamount_: Element = element.find(cbcnamespace + 'TaxAmount')
-        taxSubtotal: dict = {'taxamount': taxamount_.text,
-                             'taxamount_currencyid': taxamount_.attrib.get('currencyID')}
+        frappedoc: dict = {'taxamount': taxamount_.text,
+                           'taxamountcurrencyid': taxamount_.attrib.get('currencyID')}
+        # ['TaxCategory'] = ('cac', 'taxcategory', 'Zorunlu(1)')
+        taxcategory_: Element = element.find(cacnamespace + 'TaxCategory')
+        strategy: TRUBLCommonElement = TRUBLTaxCategory()
+        self._strategyContext.set_strategy(strategy)
+        frappedoc['taxcategory'] = self._strategyContext.return_element_data(taxcategory_,
+                                                                             cbcnamespace,
+                                                                             cacnamespace)
+        # ['CalculationSequenceNumeric'] = ('cbc', 'calculationsequencenumeric', 'Seçimli (0...1)')
+        # ['Percent'] = ('cbc', 'percent', 'Seçimli (0...1)')
         cbcsecimli01: list = ['CalculationSequenceNumeric', 'Percent']
         for elementtag_ in cbcsecimli01:
             field_: Element = element.find(cbcnamespace + elementtag_)
             if field_ is not None:
-                taxSubtotal[field_.tag.lower()] = field_.text
-
-        taxcategory_: Element = element.find(cacnamespace + 'TaxCategory')
-        strategy: TRUBLCommonElement = TRUBLTaxCategory()
-        self._strategyContext.set_strategy(strategy)
-        taxcategory = self._strategyContext.return_element_data(taxcategory_, cbcnamespace,
-                                                                cacnamespace)
-        for key in taxcategory.keys():
-            if taxcategory.get(key) is not None:
-                taxSubtotal[key] = taxcategory.get(key)
-
+                frappedoc[field_.tag.lower()] = field_.text
+        # ['TaxableAmount'] = ('cbc', 'taxableamount', 'Seçimli (0...1)')
         taxableamount_: Element = element.find(cbcnamespace + 'TaxableAmount')
         if taxableamount_ is not None:
-            taxSubtotal['taxableamount'] = taxableamount_.text
-            taxSubtotal['taxableamount_currencyid'] = taxableamount_.attrib.get('currencyID')
+            frappedoc['taxableamount'] = taxableamount_.text
+            frappedoc['taxableamountcurrencyid'] = taxableamount_.attrib.get('currencyID')
+        # ['TransactionCurrencyTaxAmount'] = ('cbc', 'transactioncurrencytaxamount', 'Seçimli (0...1)')
         transactioncurrencytaxamount_: Element = element.find(cbcnamespace + 'TransactionCurrencyTaxAmount')
         if transactioncurrencytaxamount_ is not None:
-            taxSubtotal['transactioncurrencytaxamount'] = transactioncurrencytaxamount_.text
-            taxSubtotal['transactioncurrencytaxamount_currencyid'] = transactioncurrencytaxamount_.attrib.get(
+            frappedoc['transactioncurrencytaxamount'] = transactioncurrencytaxamount_.text
+            frappedoc['transactioncurrencytaxamountcurrencyid'] = transactioncurrencytaxamount_.attrib.get(
                 'currencyID')
+        # ['BaseUnitMeasure'] = ('cbc', 'baseunitmeasure', 'Seçimli (0...1)')
         baseunitmeasure_: Element = element.find(cbcnamespace + 'BaseUnitMeasure')
         if baseunitmeasure_ is not None:
-            taxSubtotal['baseunitmeasure'] = baseunitmeasure_.text
-            taxSubtotal['baseunitmeasure_unitcode'] = baseunitmeasure_.attrib.get('unitCode')
+            frappedoc['baseunitmeasure'] = baseunitmeasure_.text
+            frappedoc['baseunitmeasureunitcode'] = baseunitmeasure_.attrib.get('unitCode')
+        # ['PerUnitAmount'] = ('cbc', 'perunitamount', 'Seçimli (0...1)')
         perunitamount_: Element = element.find(cbcnamespace + 'PerUnitAmount')
         if perunitamount_ is not None:
-            taxSubtotal['perunitamount'] = perunitamount_.text
-            taxSubtotal['perunitamount_currencyid'] = perunitamount_.attrib.get('currencyID')
+            frappedoc['perunitamount'] = perunitamount_.text
+            frappedoc['perunitamountcurrencyid'] = perunitamount_.attrib.get('currencyID')
 
         return self._get_frappedoc(self._frappeDoctype, frappedoc)
--- trebelge/TRUBLCommonElementsStrategy/TRUBLTaxTotal.py
+++ trebelge/TRUBLCommonElementsStrategy/TRUBLTaxTotal.py
@@ -1,6 +1,5 @@
 from xml.etree.ElementTree import Element
 
-import frappe
 from frappe.model.document import Document
 from trebelge.TRUBLCommonElementsStrategy.TRUBLCommonElement import TRUBLCommonElement
 from trebelge.TRUBLCommonElementsStrategy.TRUBLCommonElementContext import TRUBLCommonElementContext
@@ -8,28 +7,24 @@ from trebelge.TRUBLCommonElementsStrategy.TRUBLTaxSubtotal import TRUBLTaxSubtot
 
 
 class TRUBLTaxTotal(TRUBLCommonElement):
-    _frappeDoctype: str = 'TR UBL Tax Total'
+    _frappeDoctype: str = 'UBL TR TaxTotal'
     _strategyContext: TRUBLCommonElementContext = TRUBLCommonElementContext()
 
     def process_element(self, element: Element, cbcnamespace: str, cacnamespace: str) -> Document:
-        """
-        ['TaxAmount'] = ('cbc', 'taxamount', 'Zorunlu(1)')
-        ['currencyID'] = ('', 'taxamount_currencyid', 'Zorunlu(1)')
-        ['TaxSubtotal'] = ('cac', 'taxsubtotals', 'Zorunlu(1..n)', 'taxsubtotal')
-        """
+        # ['TaxAmount'] = ('cbc', 'taxamount', 'Zorunlu(1)')
+        # ['currencyID'] = ('', 'taxamount_currencyid', 'Zorunlu(1)')
         taxamount_: Element = element.find(cbcnamespace + 'TaxAmount')
-        taxTotal: dict = {'doctype': self._frappeDoctype,
-                          'taxamount': taxamount_.text,
-                          'taxamount_currencyid': taxamount_.attrib.get('currencyID')}
+        frappedoc: dict = {'taxamount': taxamount_.text,
+                           'taxamountcurrencyid': taxamount_.attrib.get('currencyID')}
+        # ['TaxSubtotal'] = ('cac', 'taxsubtotals', 'Zorunlu(1..n)', 'taxsubtotal')
         strategy: TRUBLCommonElement = TRUBLTaxSubtotal()
         self._strategyContext.set_strategy(strategy)
         taxsubtotals: list = []
         for taxsubtotal in element.findall(cacnamespace + 'TaxSubtotal'):
-            taxsubtotal_ = self._strategyContext.return_element_data(taxsubtotal, cbcnamespace,
+            taxsubtotal_ = self._strategyContext.return_element_data(taxsubtotal,
+                                                                     cbcnamespace,
                                                                      cacnamespace)
             taxsubtotals.append(taxsubtotal_)
-        taxTotal['taxsubtotals'] = taxsubtotals
-        newTaxTotal = frappe.get_doc(taxTotal)
-        newTaxTotal.db_insert()
+        frappedoc['taxsubtotal'] = taxsubtotals
 
-        return newTaxTotal.get_value('name')
+        return self._get_frappedoc(self._frappeDoctype, frappedoc)
