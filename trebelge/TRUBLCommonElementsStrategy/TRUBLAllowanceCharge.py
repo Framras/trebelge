420c06cba3f9d29061d49d678519d0385842be2d
toofun666 <tufan@kaynak.gen.tr>
2021-12-21 17:49:14 +0300

    Refactoring

--- trebelge/TRUBLCommonElementsStrategy/TRUBLAllowanceCharge.py
+++ trebelge/TRUBLCommonElementsStrategy/TRUBLAllowanceCharge.py
@@ -1,5 +1,6 @@
 from xml.etree.ElementTree import Element
 
+from frappe.model.document import Document
 from trebelge.TRUBLCommonElementsStrategy.TRUBLCommonElement import TRUBLCommonElement
 
 
@@ -9,12 +10,10 @@ class TRUBLAllowanceCharge(TRUBLCommonElement):
     def process_element(self, element: Element, cbcnamespace: str, cacnamespace: str) -> Document:
         # ['ChargeIndicator'] = ('cbc', 'chargeindicator', 'Zorunlu (1)')
         # ['Amount'] = ('cbc', 'allowancechargeamount', 'Zorunlu (1)')
-        # ['currencyID'] = ('', 'allowancechargeamount_currencyid', 'Zorunlu(1)')
         amount_: Element = element.find(cbcnamespace + 'Amount')
         frappedoc: dict = {'chargeindicator': element.find(cbcnamespace + 'ChargeIndicator'),
                            'amount': amount_.text,
                            'amountcurrencyid': amount_.attrib.get('currencyID')}
-
         # ['AllowanceChargeReason'] = ('cbc', 'allowancechargereason', 'Seçimli (0...1)')
         # ['MultiplierFactorNumeric'] = ('cbc', 'multiplierfactornumeric', 'Seçimli (0...1)')
         # ['SequenceNumeric'] = ('cbc', 'sequencenumeric', 'Seçimli (0...1)')
@@ -23,15 +22,12 @@ class TRUBLAllowanceCharge(TRUBLCommonElement):
             field_: Element = element.find(cbcnamespace + elementtag_)
             if field_ is not None:
                 frappedoc[field_.tag.lower()] = field_.text
-
         # ['BaseAmount'] = ('cbc', 'baseamount', 'Seçimli (0...1)')
-        # ['currencyID'] = ('', 'baseamount_currencyid', 'Zorunlu(1)')
         baseamount_: Element = element.find(cbcnamespace + 'BaseAmount')
         if baseamount_ is not None:
             frappedoc['baseamount'] = baseamount_.text
             frappedoc['baseamountcurrencyid'] = baseamount_.attrib.get('currencyID')
         # ['PerUnitAmount'] = ('cbc', 'perunitamount', 'Seçimli (0...1)')
-        # ['currencyID'] = ('', 'perunitamount_currencyid', 'Zorunlu(1)')
         perunitamount_: Element = element.find(cbcnamespace + 'PerUnitAmount')
         if perunitamount_ is not None:
             frappedoc['perunitamount'] = perunitamount_.text
