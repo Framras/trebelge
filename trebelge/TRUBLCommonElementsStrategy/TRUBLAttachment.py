6b56c521221d7740861801b8cb033745052f6763
toofun666 <tufan@kaynak.gen.tr>
2021-12-21 17:59:23 +0300

    Refactoring

--- trebelge/TRUBLCommonElementsStrategy/TRUBLAttachment.py
+++ trebelge/TRUBLCommonElementsStrategy/TRUBLAttachment.py
@@ -1,36 +1,33 @@
 from xml.etree.ElementTree import Element
 
+from frappe.model.document import Document
 from trebelge.TRUBLCommonElementsStrategy.TRUBLCommonElement import TRUBLCommonElement
 from trebelge.TRUBLCommonElementsStrategy.TRUBLCommonElementContext import TRUBLCommonElementContext
 from trebelge.TRUBLCommonElementsStrategy.TRUBLExternalReference import TRUBLExternalReference
 
 
 class TRUBLAttachment(TRUBLCommonElement):
+    _frappeDoctype: str = 'UBL TR Attachment'
     _strategyContext: TRUBLCommonElementContext = TRUBLCommonElementContext()
 
     def process_element(self, element: Element, cbcnamespace: str, cacnamespace: str) -> Document:
-        """
-        ['ExternalReference'] = ('cac', 'ExternalReference()', 'Seçimli (0..1)', 'externalreference')
-        ['EmbeddedDocumentBinaryObject'] = ('cbc', 'embeddeddocumentbinaryobject', 'Seçimli (0..1)')
-        characterSetCode: 'UTF-8'
-        filename: 'TN22021000283169.xslt'
-        encodingCode: 'Base64'
-        mimeCode: 'application/xml'
-        """
         frappedoc: dict = {}
+        # ['ExternalReference'] = ('cac', 'ExternalReference()', 'Seçimli (0..1)', 'externalreference')
         externalreference_: Element = element.find(cacnamespace + 'ExternalReference')
         if externalreference_ is not None:
             strategy: TRUBLCommonElement = TRUBLExternalReference()
             self._strategyContext.set_strategy(strategy)
-            externalreference = self._strategyContext.return_element_data(externalreference_, cbcnamespace,
-                                                                          cacnamespace)
-            for key in externalreference.keys():
-                frappedoc['externalreference_' + key] = externalreference.get(key)
+            frappedoc['externalreference'] = self._strategyContext.return_element_data(externalreference_,
+                                                                                       cbcnamespace,
+                                                                                       cacnamespace)
         # TODO implement Base64 decoder
+        # ['EmbeddedDocumentBinaryObject'] = ('cbc', 'embeddeddocumentbinaryobject', 'Seçimli (0..1)')
+        # characterSetCode: 'UTF-8'
+        # filename: 'TN22021000283169.xslt'
+        # encodingCode: 'Base64'
+        # mimeCode: 'application/xml'
         embeddeddocumentbinaryobject_: Element = element.find(cacnamespace + 'EmbeddedDocumentBinaryObject')
         if embeddeddocumentbinaryobject_ is not None:
-            for key in embeddeddocumentbinaryobject_.attrib.keys():
-                frappedoc[('EmbeddedDocumentBinaryObject_' + key).lower()] = embeddeddocumentbinaryobject_.attrib.get(
-                    key)
+            pass
 
         return self._get_frappedoc(self._frappeDoctype, frappedoc)
--- trebelge/TRUBLCommonElementsStrategy/TRUBLExternalReference.py
+++ trebelge/TRUBLCommonElementsStrategy/TRUBLExternalReference.py
@@ -1,13 +1,14 @@
 from xml.etree.ElementTree import Element
 
+from frappe.model.document import Document
 from trebelge.TRUBLCommonElementsStrategy.TRUBLCommonElement import TRUBLCommonElement
 
 
 class TRUBLExternalReference(TRUBLCommonElement):
+    _frappeDoctype: str = 'UBL TR ExternalReference'
+
     def process_element(self, element: Element, cbcnamespace: str, cacnamespace: str) -> Document:
-        """
-        ['URI'] = ('cbc', '', 'Zorunlu(1)')
-        """
-        externalreference: dict = {'uri': element.find(cbcnamespace + 'URI').text}
+        # ['URI'] = ('cbc', '', 'Zorunlu(1)')
+        frappedoc: dict = {'uri': element.find(cbcnamespace + 'URI').text}
 
         return self._get_frappedoc(self._frappeDoctype, frappedoc)
