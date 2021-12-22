2fabc145d3804f0d2a8a70d220c8e163e7bf8741
toofun666 <tufan@kaynak.gen.tr>
2021-12-22 12:16:17 +0300

    Refactoring

--- trebelge/TRUBLCommonElementsStrategy/TRUBLShipmentStage.py
+++ trebelge/TRUBLCommonElementsStrategy/TRUBLShipmentStage.py
@@ -3,10 +3,13 @@ from xml.etree.ElementTree import Element
 from frappe.model.document import Document
 from trebelge.TRUBLCommonElementsStrategy.TRUBLCommonElement import TRUBLCommonElement
 from trebelge.TRUBLCommonElementsStrategy.TRUBLCommonElementContext import TRUBLCommonElementContext
+from trebelge.TRUBLCommonElementsStrategy.TRUBLPeriod import TRUBLPeriod
+from trebelge.TRUBLCommonElementsStrategy.TRUBLPerson import TRUBLPerson
+from trebelge.TRUBLCommonElementsStrategy.TRUBLTransportMeans import TRUBLTransportMeans
 
 
 class TRUBLShipmentStage(TRUBLCommonElement):
-    _frappeDoctype: str = 'UBL TR Price'
+    _frappeDoctype: str = 'UBL TR ShipmentStage'
     _strategyContext: TRUBLCommonElementContext = TRUBLCommonElementContext()
 
     def process_element(self, element: Element, cbcnamespace: str, cacnamespace: str) -> Document:
@@ -15,87 +18,44 @@ class TRUBLShipmentStage(TRUBLCommonElement):
         # ['TransportModeCode'] = ('cbc', '', 'Seçimli (0...1)')
         # ['TransportMeansTypeCode'] = ('cbc', '', 'Seçimli (0...1)')
         # ['Instructions'] = ('cbc', '', 'Seçimli (0...1)')
-
-        # ['TransitDirectionCode'] = ('cbc', '', 'Seçimli (0...n)')
-
-        # ['TransitPeriod'] = ('cac', 'Period', 'Seçimli (0...1)')
-        # ['TransportMeans'] = ('cac', 'TransportMeans', 'Seçimli (0...1)')
-        # ['DriverPerson'] = ('cac', 'Person', 'Seçimli (0...n)')
-
-        cbcsecimli01: list = ['HandlingCode', 'HandlingInstructions']
+        cbcsecimli01: list = ['ID', 'TransportModeCode', 'TransportMeansTypeCode', 'Instructions']
         for elementtag_ in cbcsecimli01:
             field_: Element = element.find(cbcnamespace + elementtag_)
             if field_:
                 frappedoc[field_.tag.lower()] = field_.text
-
-        # ['GrossWeightMeasure'] = ('cbc', '', 'Seçimli (0...1)')
-        grossweightmeasure_: Element = element.find(cbcnamespace + 'GrossWeightMeasure')
-        if grossweightmeasure_:
-            frappedoc['grossweightmeasure'] = grossweightmeasure_.text
-            frappedoc['grossweightmeasureunitcode'] = grossweightmeasure_.attrib.get('unitCode')
-        # ['NetWeightMeasure'] = ('cbc', '', 'Seçimli (0...1)')
-        netweightmeasure_: Element = element.find(cbcnamespace + 'NetWeightMeasure')
-        if netweightmeasure_:
-            frappedoc['netweightmeasure'] = netweightmeasure_.text
-            frappedoc['netweightmeasureunitcode'] = netweightmeasure_.attrib.get('unitCode')
-        # ['GrossVolumeMeasure'] = ('cbc', '', 'Seçimli (0...1)')
-        grossvolumemeasure_: Element = element.find(cbcnamespace + 'GrossVolumeMeasure')
-        if grossvolumemeasure_:
-            frappedoc['grossvolumemeasure'] = grossvolumemeasure_.text
-            frappedoc['grossvolumemeasureunitcode'] = grossvolumemeasure_.attrib.get('unitCode')
-        # ['NetVolumeMeasure'] = ('cbc', '', 'Seçimli (0...1)')
-        netvolumemeasure_: Element = element.find(cbcnamespace + 'NetVolumeMeasure')
-        if netvolumemeasure_:
-            frappedoc['netvolumemeasure'] = netvolumemeasure_.text
-            frappedoc['netvolumemeasureunitcode'] = netvolumemeasure_.attrib.get('unitCode')
-        # ['TotalGoodsItemQuantity'] = ('cbc', '', 'Seçimli (0...1)')
-        totalgoodsitemquantity_: Element = element.find(cbcnamespace + 'TotalGoodsItemQuantity')
-        if totalgoodsitemquantity_:
-            frappedoc['totalgoodsitemquantity'] = totalgoodsitemquantity_.text
-            frappedoc['totalgoodsitemquantityunitcode'] = totalgoodsitemquantity_.attrib.get('unitCode')
-        # ['TotalTransportHandlingUnitQuantity'] = ('cbc', '', 'Seçimli (0...1)')
-        totaltransporthandlingunitquantity_: Element = element.find(cbcnamespace + 'TotalTransportHandlingUnitQuantity')
-        if totaltransporthandlingunitquantity_:
-            frappedoc['totaltransporthandlingunitquantity'] = totaltransporthandlingunitquantity_.text
-            frappedoc['totaltransporthandlingunitquantityunitcode'] = totaltransporthandlingunitquantity_.attrib.get(
-                'unitCode')
-        # ['InsuranceValueAmount'] = ('cbc', '', 'Seçimli (0...1)')
-        insurancevalueamount_: Element = element.find(cbcnamespace + 'InsuranceValueAmount')
-        if insurancevalueamount_:
-            frappedoc['insurancevalueamount'] = insurancevalueamount_.text
-            frappedoc['insurancevalueamountcurrencyid'] = insurancevalueamount_.attrib.get('currencyID')
-        # ['DeclaredCustomsValueAmount'] = ('cbc', '', 'Seçimli (0...1)')
-        declaredcustomsvalueamount_: Element = element.find(cbcnamespace + 'DeclaredCustomsValueAmount')
-        if declaredcustomsvalueamount_:
-            frappedoc['declaredcustomsvalueamount'] = declaredcustomsvalueamount_.text
-            frappedoc['declaredcustomsvalueamountcurrencyid'] = declaredcustomsvalueamount_.attrib.get('currencyID')
-        # ['DeclaredForCarriageValueAmount'] = ('cbc', '', 'Seçimli (0...1)')
-        declaredforcarriagevalueamount_: Element = element.find(cbcnamespace + 'DeclaredCustomsValueAmount')
-        if declaredforcarriagevalueamount_:
-            frappedoc['declaredforcarriagevalueamount'] = declaredforcarriagevalueamount_.text
-            frappedoc['declaredforcarriagevalueamountcurrencyid'] = declaredforcarriagevalueamount_.attrib.get(
-                'currencyID')
-        # ['DeclaredStatisticsValueAmount'] = ('cbc', '', 'Seçimli (0...1)')
-        declaredstatisticsvalueamount_: Element = element.find(cbcnamespace + 'DeclaredStatisticsValueAmount')
-        if declaredstatisticsvalueamount_:
-            frappedoc['declaredstatisticsvalueamount'] = declaredstatisticsvalueamount_.text
-            frappedoc['declaredstatisticsvalueamountcurrencyid'] = declaredstatisticsvalueamount_.attrib.get(
-                'currencyID')
-        # ['FreeOnBoardValueAmount'] = ('cbc', '', 'Seçimli (0...1)')
-        freeonboardvalueamount_: Element = element.find(cbcnamespace + 'FreeOnBoardValueAmount')
-        if freeonboardvalueamount_:
-            frappedoc['freeonboardvalueamount'] = freeonboardvalueamount_.text
-            frappedoc['freeonboardvalueamountcurrencyid'] = freeonboardvalueamount_.attrib.get('currencyID')
-
-        # ['SpecialInstructions'] = ('cbc', '', 'Seçimli (0...n)')
-
-        # ['Delivery'] = ('cac', 'Delivery', 'Seçimli (0...1)')
-        # ['ReturnAddress'] = ('cac', 'Address', 'Seçimli (0...1)')
-        # ['FirstArrivalPortLocation'] = ('cac', 'Location', 'Seçimli (0...1)')
-        # ['LastExitPortLocation'] = ('cac', 'Location', 'Seçimli (0...1)')
-
-        # ['GoodsItem'] = ('cac', 'GoodsItem', 'Seçimli (0...n)')
-        # ['ShipmentStage'] = ('cac', 'ShipmentStage', 'Seçimli (0...n)')
-        # ['TransportHandlingUnit'] = ('cac', 'TransportHandlingUnit', 'Seçimli (0...n)')
+        # ['TransitDirectionCode'] = ('cbc', '', 'Seçimli (0...n)')
+        transitdirectioncodes_: list = element.findall(cbcnamespace + 'TransitDirectionCode')
+        if transitdirectioncodes_:
+            transitdirectioncode: list = []
+            for transitdirectioncode_ in transitdirectioncodes_:
+                transitdirectioncode.append(transitdirectioncode_.text)
+            frappedoc['transitdirectioncode'] = transitdirectioncode
+        # ['TransitPeriod'] = ('cac', 'Period', 'Seçimli (0...1)')
+        transitperiod_: Element = element.find(cbcnamespace + 'TransitPeriod')
+        if transitperiod_:
+            strategy: TRUBLCommonElement = TRUBLPeriod()
+            self._strategyContext.set_strategy(strategy)
+            frappedoc['transitperiod'] = [self._strategyContext.return_element_data(transitperiod_,
+                                                                                    cbcnamespace,
+                                                                                    cacnamespace)]
+        # ['TransportMeans'] = ('cac', 'TransportMeans', 'Seçimli (0...1)')
+        transportmeans_: Element = element.find(cbcnamespace + 'TransportMeans')
+        if transportmeans_:
+            strategy: TRUBLCommonElement = TRUBLTransportMeans()
+            self._strategyContext.set_strategy(strategy)
+            frappedoc['transportmeans'] = [self._strategyContext.return_element_data(transportmeans_,
+                                                                                     cbcnamespace,
+                                                                                     cacnamespace)]
+        # ['DriverPerson'] = ('cac', 'Person', 'Seçimli (0...n)')
+        driverpeople_: list = element.findall(cacnamespace + 'DriverPerson')
+        if driverpeople_:
+            driverpeople: list = []
+            strategy: TRUBLCommonElement = TRUBLPerson()
+            self._strategyContext.set_strategy(strategy)
+            for driverperson_ in driverpeople_:
+                driverpeople.append(self._strategyContext.return_element_data(driverperson_,
+                                                                              cbcnamespace,
+                                                                              cacnamespace))
+            frappedoc['driverperson'] = driverpeople
 
         return self._get_frappedoc(self._frappeDoctype, frappedoc)
--- trebelge/TRUBLCommonElementsStrategy/TRUBLSupplierParty.py
+++ trebelge/TRUBLCommonElementsStrategy/TRUBLSupplierParty.py
@@ -1,5 +1,6 @@
 from xml.etree.ElementTree import Element
 
+from frappe.model.document import Document
 from trebelge.TRUBLCommonElementsStrategy.TRUBLCommonElement import TRUBLCommonElement
 from trebelge.TRUBLCommonElementsStrategy.TRUBLCommonElementContext import TRUBLCommonElementContext
 from trebelge.TRUBLCommonElementsStrategy.TRUBLContact import TRUBLContact
@@ -7,6 +8,7 @@ from trebelge.TRUBLCommonElementsStrategy.TRUBLParty import TRUBLParty
 
 
 class TRUBLSupplierParty(TRUBLCommonElement):
+    _frappeDoctype: str = 'UBL TR SupplierParty'
     _strategyContext: TRUBLCommonElementContext = TRUBLCommonElementContext()
 
     def process_element(self, element: Element, cbcnamespace: str, cacnamespace: str) -> Document:
@@ -15,16 +17,16 @@ class TRUBLSupplierParty(TRUBLCommonElement):
         party_: Element = element.find(cacnamespace + 'Party')
         strategy: TRUBLCommonElement = TRUBLParty()
         self._strategyContext.set_strategy(strategy)
-        party = self._strategyContext.return_element_data(party_, cbcnamespace, cacnamespace)
-        for key in party.keys():
-            frappedoc['party_' + key] = party.get(key)
+        frappedoc['party'] = self._strategyContext.return_element_data(party_,
+                                                                       cbcnamespace,
+                                                                       cacnamespace)
         # ['DespatchContact'] = ('cac', 'Contact()', 'Seçimli(0..1)', 'despatchcontact')
         despatchcontact_: Element = element.find(cacnamespace + 'DespatchContact')
         if despatchcontact_:
             strategy: TRUBLCommonElement = TRUBLContact()
             self._strategyContext.set_strategy(strategy)
-            despatchcontact = self._strategyContext.return_element_data(despatchcontact_, cbcnamespace, cacnamespace)
-            for key in despatchcontact.keys():
-                frappedoc['despatchcontact_' + key] = despatchcontact.get(key)
+            frappedoc['despatchcontact'] = self._strategyContext.return_element_data(despatchcontact_,
+                                                                                     cbcnamespace,
+                                                                                     cacnamespace)
 
         return self._get_frappedoc(self._frappeDoctype, frappedoc)
