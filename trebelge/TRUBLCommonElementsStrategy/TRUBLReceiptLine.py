9b8ac8cce3d24ca04a02d849d9672ca0bc0ec3a5
toofun666 <tufan@kaynak.gen.tr>
2021-12-22 14:07:51 +0300

    Refactoring

--- trebelge/TRUBLCommonElementsStrategy/TRUBLReceiptLine.py
+++ trebelge/TRUBLCommonElementsStrategy/TRUBLReceiptLine.py
@@ -10,21 +10,36 @@ class TRUBLReceiptLine(TRUBLCommonElement):
     _strategyContext: TRUBLCommonElementContext = TRUBLCommonElementContext()
 
     def process_element(self, element: Element, cbcnamespace: str, cacnamespace: str) -> Document:
-        frappedoc: dict = {}
         # ['ID'] = ('cbc', '', 'Zorunlu (1)')
-        # ['ReceivedQuantity'] = ('cbc', '', 'Seçimli (0...1)')
-        # ['ShortQuantity'] = ('cbc', '', 'Seçimli (0...1)')
-        # ['RejectedQuantity'] = ('cbc', '', 'Seçimli (0...1)')
+        frappedoc: dict = {'id': element.find(cbcnamespace + 'ID').text}
         # ['RejectReasonCode'] = ('cbc', '', 'Seçimli (0...1)')
-        # ['OversupplyQuantity'] = ('cbc', '', 'Seçimli (0...1)')
         # ['ReceivedDate'] = ('cbc', '', 'Seçimli (0...1)')
         # ['TimingComplaintCode'] = ('cbc', '', 'Seçimli (0...1)')
         # ['TimingComplaint'] = ('cbc', '', 'Seçimli (0...1)')
+        cbcsecimli01: list = ['RejectReasonCode', 'ReceivedDate', 'TimingComplaintCode', 'TimingComplaint']
+        for elementtag_ in cbcsecimli01:
+            field_: Element = element.find(cbcnamespace + elementtag_)
+            if field_:
+                frappedoc[field_.tag.lower()] = field_.text
+        # ['ReceivedQuantity'] = ('cbc', '', 'Seçimli (0...1)')
+        # ['ShortQuantity'] = ('cbc', '', 'Seçimli (0...1)')
+        # ['RejectedQuantity'] = ('cbc', '', 'Seçimli (0...1)')
+        # ['OversupplyQuantity'] = ('cbc', '', 'Seçimli (0...1)')
+        cbcqtysecimli01: list = ['ReceivedQuantity', 'ShortQuantity', 'RejectedQuantity', 'OversupplyQuantity']
+        for elementtag_ in cbcqtysecimli01:
+            field_: Element = element.find(cbcnamespace + elementtag_)
+            if field_:
+                frappedoc[field_.tag.lower()] = field_.text
+                frappedoc[field_.tag.lower() + 'unitcode'] = field_.attrib.get('unitCode')
         # ['Note'] = ('cbc', '', 'Seçimli (0...n)')
+
         # ['RejectReason'] = ('cbc', '', 'Seçimli (0...n)')
+
         # ['Item'] = ('cac', 'Item', 'Zorunlu (1)')
+
         # ['OrderLineReference'] = ('cac', 'OrderLineReference', 'Seçimli (0...1)')
         # ['DespatchLineReference'] = ('cac', 'LineReference', 'Seçimli (0...1)')
+
         # ['DocumentReference'] = ('cac', 'DocumentReference', 'Seçimli (0...n)')
         # ['Shipment'] = ('cac', 'Shipment', 'Seçimli (0...n)')
 
