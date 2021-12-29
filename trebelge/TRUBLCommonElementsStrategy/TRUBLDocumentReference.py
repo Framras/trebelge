129d682b71a351bd71c0943f156a54e36f0cdcde
toofun666 <tufan@kaynak.gen.tr>
2021-12-30 00:29:36 +0300

    Refactoring

--- trebelge/TRUBLCommonElementsStrategy/TRUBLDocumentReference.py
+++ trebelge/TRUBLCommonElementsStrategy/TRUBLDocumentReference.py
@@ -28,27 +28,27 @@ class TRUBLDocumentReference(TRUBLCommonElement):
         # ['IssuerParty'] = ('cac', 'Party', 'Seçimli (0...1)', 'issuerparty')
         attachment_ = element.find('./' + cacnamespace + 'Attachment')
         if attachment_:
-            frappedoc['attachment'] = [TRUBLAttachment.process_element(attachment_,
-                                                                       cbcnamespace,
-                                                                       cacnamespace)]
+            frappedoc['attachment'] = TRUBLAttachment().process_element(attachment_,
+                                                                        cbcnamespace,
+                                                                        cacnamespace).name
         validityperiod_ = element.find('./' + cacnamespace + 'ValidityPeriod')
         if validityperiod_:
-            frappedoc['validityperiod'] = [TRUBLPeriod.process_element(validityperiod_,
-                                                                       cbcnamespace,
-                                                                       cacnamespace)]
+            frappedoc['validityperiod'] = TRUBLPeriod().process_element(validityperiod_,
+                                                                        cbcnamespace,
+                                                                        cacnamespace).name
         issuerparty_ = element.find('./' + cacnamespace + 'IssuerParty')
         if issuerparty_:
-            frappedoc['issuerparty'] = [TRUBLParty.process_element(issuerparty_,
-                                                                   cbcnamespace,
-                                                                   cacnamespace)]
+            frappedoc['issuerparty'] = TRUBLParty().process_element(issuerparty_,
+                                                                    cbcnamespace,
+                                                                    cacnamespace).name
         # ['DocumentDescription'] = ('cbc', '', 'Seçimli(0..n)', 'documentdescription')
         documentdescriptions_: list = element.findall('./' + cbcnamespace + 'DocumentDescription')
         if len(documentdescriptions_) != 0:
             documentdescriptions: list = []
             for documentdescription_ in documentdescriptions_:
-                documentdescriptions.append(TRUBLNote.process_element(documentdescription_,
-                                                                      cbcnamespace,
-                                                                      cacnamespace))
+                documentdescriptions.append(TRUBLNote().process_element(documentdescription_,
+                                                                        cbcnamespace,
+                                                                        cacnamespace))
             frappedoc['documentdescription'] = documentdescriptions
 
         return self._get_frappedoc(self._frappeDoctype, frappedoc)
