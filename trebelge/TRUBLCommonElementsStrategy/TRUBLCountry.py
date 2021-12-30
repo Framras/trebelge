eece2f00e4fd6bfbe00294cd07932ec5489bfaeb
toofun666 <tufan@kaynak.gen.tr>
2021-12-30 18:15:16 +0300

    Refactoring

--- trebelge/TRUBLCommonElementsStrategy/TRUBLCountry.py
+++ trebelge/TRUBLCommonElementsStrategy/TRUBLCountry.py
@@ -1,5 +1,6 @@
 from xml.etree.ElementTree import Element
 
+import frappe
 from frappe.model.document import Document
 from trebelge.TRUBLCommonElementsStrategy.TRUBLCommonElement import TRUBLCommonElement
 
@@ -9,7 +10,14 @@ class TRUBLCountry(TRUBLCommonElement):
 
     def process_element(self, element: Element, cbcnamespace: str, cacnamespace: str) -> Document:
         # ['Name'] = ('cbc', 'countryname', 'Zorunlu(1)')
-        frappedoc: dict = {'countryname': element.find('./' + cbcnamespace + 'Name').text}
+        frappedoc: dict = {}
+        countryname = element.find('./' + cbcnamespace + 'Name')
+        if countryname:
+            frappedoc['countryname'] = countryname.text
+        else:
+            frappe.log_error('countryname not provided for ' + element.tag, 'TRUBLCountry')
+            frappedoc['countryname'] = str('')
+
         # ['IdentificationCode'] = ('cbc', 'identificationcode', 'Seçimli (0...1)')
         identificationcode_: Element = element.find('./' + cbcnamespace + 'IdentificationCode')
         if identificationcode_ is not None:
