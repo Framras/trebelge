104de1028a9834776cb0dd4d0b0120c5a5e83a4f
toofun666 <tufan@kaynak.gen.tr>
2021-12-31 13:44:49 +0300

    Refactoring

--- trebelge/TRUBLCommonElementsStrategy/TRUBLItemIdentification.py
+++ trebelge/TRUBLCommonElementsStrategy/TRUBLItemIdentification.py
@@ -1,5 +1,6 @@
 from xml.etree.ElementTree import Element
 
+import frappe
 from frappe.model.document import Document
 from trebelge.TRUBLCommonElementsStrategy.TRUBLCommonElement import TRUBLCommonElement
 
@@ -8,7 +9,13 @@ class TRUBLItemIdentification(TRUBLCommonElement):
     _frappeDoctype: str = 'UBL TR ItemIdentification'
 
     def process_element(self, element: Element, cbcnamespace: str, cacnamespace: str) -> Document:
+        frappedoc: dict = {}
         # ['ID'] = ('cbc', 'id', 'Zorunlu(1)')
-        frappedoc: dict = {'id': element.find('./' + cbcnamespace + 'ID').text}
+        id_ = element.find('./' + cbcnamespace + 'ID')
+        if id_:
+            frappedoc['id'] = id_.text
+        else:
+            frappe.log_error('id not provided for ' + element.tag, 'TRUBLItemIdentification')
+            frappedoc['id'] = str('-')
 
         return self._get_frappedoc(self._frappeDoctype, frappedoc)
