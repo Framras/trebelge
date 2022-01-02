f6e3f52152c314b7470724218d2f37495cc95362
toofun666 <tufan@kaynak.gen.tr>
2022-01-02 23:37:15 +0300

    Refactoring

--- trebelge/TRUBLCommonElementsStrategy/TRUBLPerson.py
+++ trebelge/TRUBLCommonElementsStrategy/TRUBLPerson.py
@@ -11,9 +11,14 @@ class TRUBLPerson(TRUBLCommonElement):
 
     def process_element(self, element: Element, cbcnamespace: str, cacnamespace: str) -> Document:
         # ['FirstName'] = ('cbc', 'firstname', 'Zorunlu(1)')
+        firstname_ = element.find('./' + cbcnamespace + 'FirstName').text
         # ['FamilyName'] = ('cbc', 'familyname', 'Zorunlu(1)')
-        frappedoc: dict = {'firstname': element.find('./' + cbcnamespace + 'FirstName').text,
-                           'familyname': element.find('./' + cbcnamespace + 'FamilyName').text}
+        familyname_ = element.find('./' + cbcnamespace + 'FamilyName').text
+        if firstname_ is not None and familyname_ is not None:
+            frappedoc: dict = {'firstname': firstname_,
+                               'familyname': familyname_}
+        else:
+            return None
         # ['MiddleName'] = ('cbc', '', 'Seçimli (0...1)')
         # ['NameSuffix'] = ('cbc', '', 'Seçimli (0...1)')
         # ['NationalityID'] = ('cbc', '', 'Seçimli (0...1)')
@@ -21,22 +26,24 @@ class TRUBLPerson(TRUBLCommonElement):
         for elementtag_ in cbcsecimli01:
             field_: Element = element.find('./' + cbcnamespace + elementtag_)
             if field_ is not None:
-                frappedoc[elementtag_.lower()] = field_.text
+                if field_.text is not None:
+                    frappedoc[elementtag_.lower()] = field_.text
         # ['Title'] = ('cbc', 'persontitle', 'Seçimli (0...1)')
         field_: Element = element.find('./' + cbcnamespace + 'Title')
         if field_ is not None:
-            frappedoc['persontitle'] = field_.text
+            if field_.text is not None:
+                frappedoc['persontitle'] = field_.text
         # ['FinancialAccount'] = ('cac', 'FinancialAccount', 'Seçimli (0...1)', 'financialaccount')
         financialaccount_: Element = element.find('./' + cacnamespace + 'FinancialAccount')
         if financialaccount_ is not None:
-            frappedoc['financialaccount'] = TRUBLFinancialAccount().process_element(financialaccount_,
-                                                                                    cbcnamespace,
-                                                                                    cacnamespace).name
+            tmp = TRUBLFinancialAccount().process_element(financialaccount_, cbcnamespace, cacnamespace)
+            if tmp is not None:
+                frappedoc['financialaccount'] = tmp.name
         # ['IdentityDocumentReference'] = ('cac', 'DocumentReference', 'Seçimli (0...1)', 'documentreference')
         documentreference_: Element = element.find('./' + cacnamespace + 'IdentityDocumentReference')
         if documentreference_ is not None:
-            frappedoc['documentreference'] = TRUBLDocumentReference().process_element(documentreference_,
-                                                                                      cbcnamespace,
-                                                                                      cacnamespace).name
+            tmp = TRUBLDocumentReference().process_element(documentreference_, cbcnamespace, cacnamespace)
+            if tmp is not None:
+                frappedoc['documentreference'] = tmp.name
 
         return self._get_frappedoc(self._frappeDoctype, frappedoc)
