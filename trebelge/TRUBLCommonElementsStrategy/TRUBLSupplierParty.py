4ff9a3d7e2d1811fd99df4931f56cf47ad9b262d
toofun666 <tufan@kaynak.gen.tr>
2022-01-06 13:21:25 +0300

    Refactoring

--- trebelge/TRUBLCommonElementsStrategy/TRUBLSupplierParty.py
+++ trebelge/TRUBLCommonElementsStrategy/TRUBLSupplierParty.py
@@ -3,7 +3,6 @@ from xml.etree.ElementTree import Element
 from frappe.model.document import Document
 from trebelge.TRUBLCommonElementsStrategy.TRUBLCommonElement import TRUBLCommonElement
 from trebelge.TRUBLCommonElementsStrategy.TRUBLContact import TRUBLContact
-from trebelge.TRUBLCommonElementsStrategy.TRUBLParty import TRUBLParty
 
 
 class TRUBLSupplierParty(TRUBLCommonElement):
