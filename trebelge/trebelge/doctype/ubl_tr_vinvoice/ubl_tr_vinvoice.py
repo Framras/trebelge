50a112b7c6f9b6410e816f8690518108873727af
toofun666 <tufan@kaynak.gen.tr>
2022-01-08 22:54:38 +0300

    Refactoring

--- trebelge/trebelge/doctype/ubl_tr_vinvoice/ubl_tr_vinvoice.py
+++ trebelge/trebelge/doctype/ubl_tr_vinvoice/ubl_tr_vinvoice.py
@@ -23,7 +23,7 @@ class UBLTRVInvoice(Document):
         pass
 
     def get_list(self, args):
-        uuid_: list = []
+        uuid: list = []
         # for all *.xml files
         for xmlFile in frappe.get_all('File', filters={"file_name": ["like", "%.xml"], "is_folder": 0},
                                       fields={"file_url"}):
@@ -37,7 +37,7 @@ class UBLTRVInvoice(Document):
                 _cac_ns = str('{' + _namespaces.get('cac') + '}')
                 _cbc_ns = str('{' + _namespaces.get('cbc') + '}')
                 root_: Element = ET.parse(filePath).getroot()
-                uuid_.append(root_.find('./' + _cbc_ns + 'UUID').text)
-        if len(uuid_) == 0:
+                uuid.append(dict(uuid=root_.find('./' + _cbc_ns + 'UUID').text))
+        if len(uuid) == 0:
             return None
-        return uuid_
+        return uuid
