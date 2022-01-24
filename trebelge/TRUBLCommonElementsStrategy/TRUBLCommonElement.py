1db393160ffd93ecda82632bb1bce39e0598fddb
toofun666 <tufan@kaynak.gen.tr>
2022-01-24 20:42:52 +0300

    Refactoring

--- trebelge/TRUBLCommonElementsStrategy/TRUBLCommonElement.py
+++ trebelge/TRUBLCommonElementsStrategy/TRUBLCommonElement.py
@@ -13,7 +13,10 @@ class TRUBLCommonElement(ABC):
     The Context uses this interface to call the algorithm defined by Concrete
     Strategies.
     """
-    nullcriteria = 'is null'
+
+    @property
+    def nullcriteria(self) -> str:
+        return 'is null'
 
     @staticmethod
     def _get_frappedoc(frappedoctype: str, frappedoc: dict, leaf: bool = True) -> Document:
