23c223129011bd0d6458444af33eee06cf24a2d7
toofun666 <tufan@kaynak.gen.tr>
2022-01-26 18:34:58 +0300

    Refactoring

--- trebelge/TRUBLCommonElementsStrategy/TRUBLFinancialAccount.py
+++ trebelge/TRUBLCommonElementsStrategy/TRUBLFinancialAccount.py
@@ -9,26 +9,27 @@ class TRUBLFinancialAccount(TRUBLCommonElement):
     _frappeDoctype = 'UBL TR FinancialAccount'
 
     def process_element(self, element: Element, cbcnamespace: str, cacnamespace: str) -> Document:
+        frappedoc: dict = {}
         # ['ID'] = ('cbc', 'id', 'Zorunlu(1)')
         id_: Element = element.find('./' + cbcnamespace + 'ID')
-        if id_ is None or id_.text.strip() == '':
-            return None
-        if id_.text.strip() == '':
-            frappedoc: dict = {'id': 'girilmemiştir'}
-        else:
-            frappedoc: dict = {'id': id_.text}
+        if id_ is not None:
+            if id_.text is not None:
+                frappedoc['id'] = id_.text.strip()
         # ['CurrencyCode'] = ('cbc', 'currencycode', 'Seçimli (0...1)')
         # ['PaymentNote'] = ('cbc', 'paymentnote', 'Seçimli (0...1)')
         cbcsecimli01: list = ['CurrencyCode', 'PaymentNote']
         for elementtag_ in cbcsecimli01:
             field_: Element = element.find('./' + cbcnamespace + elementtag_)
-            if field_ is not None and field_.text.strip() != '':
-                frappedoc[elementtag_.lower()] = field_.text
+            if field_ is not None:
+                if field_.text is not None:
+                    frappedoc[elementtag_.lower()] = field_.text.strip()
         # ['FinancialInstitutionBranch'] = ('cac', 'Branch()', 'Seçimli (0...1)', 'financialinstitutionbranch')
         financialinstitutionbranch_: Element = element.find('./' + cacnamespace + 'FinancialInstitutionBranch')
         if financialinstitutionbranch_ is not None:
             tmp = TRUBLBranch().process_element(financialinstitutionbranch_, cbcnamespace, cacnamespace)
             if tmp is not None:
                 frappedoc['financialinstitutionbranch'] = tmp.name
+        if frappedoc == {}:
+            return None
 
         return self._get_frappedoc(self._frappeDoctype, frappedoc)
