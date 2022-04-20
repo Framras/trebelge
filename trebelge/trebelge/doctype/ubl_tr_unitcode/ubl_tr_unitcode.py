eef4a184ad03a1077a18004a29a43378259d4be6
framras <tufan.kaynak@framras.com>
2022-04-21 00:32:01 +0300

    Credit Note coming to EBelge

--- trebelge/trebelge/doctype/ubl_tr_unitcode/ubl_tr_unitcode.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, Framras AS-Izmir and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class UBLTRUnitcode(Document):
-	pass
