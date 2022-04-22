a1658c47d168b5ac341f68f7eb611c532dbf4802
framras <tufan.kaynak@framras.com>
2022-04-22 12:26:01 +0300

    Denormalizing Notes as Child Table

--- trebelge/TRUBLCommonElementsStrategy/TRUBLPeriod.py
+++ trebelge/TRUBLCommonElementsStrategy/TRUBLPeriod.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from xml.etree.ElementTree import Element
 
 from frappe.model.document import Document
@@ -14,12 +15,26 @@ class TRUBLPeriod(TRUBLCommonElement):
         # ['EndDate'] = ('cbc', 'enddate', 'Seçimli (0...1)')
         # ['EndTime'] = ('cbc', 'endtime', 'Seçimli (0...1)')
         # ['Description'] = ('cbc', 'description', 'Seçimli (0...1)')
-        cbcsecimli01: list = ['StartDate', 'StartTime', 'EndDate', 'EndTime', 'Description']
+        cbcsecimli01: list = ['StartDate', 'EndDate', 'Description']
         for elementtag_ in cbcsecimli01:
             field_: Element = element.find('./' + cbcnamespace + elementtag_)
             if field_ is not None:
                 if field_.text is not None:
                     frappedoc[elementtag_.lower()] = field_.text.strip()
+        # ['StartTime'] = ('cbc', '', 'Seçimli (0...1)')
+        starttime_: Element = element.find('./' + cbcnamespace + 'StartTime')
+        if starttime_ is not None:
+            try:
+                frappedoc['starttime'] = datetime.strptime(starttime_.text, '%H:%M:%S')
+            except ValueError:
+                pass
+        # ['EndTime'] = ('cbc', '', 'Seçimli (0...1)')
+        endtime_: Element = element.find('./' + cbcnamespace + 'EndTime')
+        if endtime_ is not None:
+            try:
+                frappedoc['endtime'] = datetime.strptime(endtime_.text, '%H:%M:%S')
+            except ValueError:
+                pass
         # ['DurationMeasure'] = ('cbc', 'durationmeasure', 'Seçimli (0...1)')
         durationmeasure_: Element = element.find('./' + cbcnamespace + 'DurationMeasure')
         if durationmeasure_ is not None:
