5908dbc077e8ae8f4f44814882043f313807d081
framras <tufan.kaynak@framras.com>
2022-05-04 14:42:45 +0300

    Denormalizing Notes as Child Table

---
+++ trebelge/trebelge/doctype/ubl_tr_invoice/ubl_tr_invoice.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Framras AS-Izmir and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('UBL TR Invoice', {
+	// refresh: function(frm) {
+
+	// }
+});
