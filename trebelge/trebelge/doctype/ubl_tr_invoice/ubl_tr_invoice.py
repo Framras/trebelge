9d4cb176280fce306d914f5009835e4a15da6218
framras <tufan.kaynak@framras.com>
2022-05-04 16:23:10 +0300

    Denormalizing Notes as Child Table

---
+++ trebelge/trebelge/doctype/ubl_tr_invoice/ubl_tr_invoice.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Framras AS-Izmir and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class UBLTRInvoice(Document):
+	pass
