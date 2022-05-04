042bb0c240e8f44924463012a6abaf5b72757ffc
framras <tufan.kaynak@framras.com>
2022-05-04 17:38:58 +0300

    Denormalizing Notes as Child Table

--- trebelge/TRUBLCommonElementsStrategy/TRUBLPeriod.py
+++ trebelge/TRUBLCommonElementsStrategy/TRUBLPeriod.py
@@ -42,7 +42,4 @@ class TRUBLPeriod(TRUBLCommonElement):
             frappedata['durationmeasure'] = durationmeasure_.text.strip()
             frappedata['durationmeasure_unitcode'] = durationmeasure_.attrib.get('unitCode').strip()
 
-        if frappedata == {}:
-            return None
-        else:
-            return frappedata
+        return frappedata
