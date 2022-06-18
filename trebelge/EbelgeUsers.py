d74753a2aaa4ff8dafe0ba2c2e5c47c5b5970cc0
framras <tufan.kaynak@framras.com>
2022-06-18 14:05:15 +0300

    Initializing İFL

--- trebelge/EbelgeUsers.py
+++ trebelge/EbelgeUsers.py
@@ -2,8 +2,8 @@ class EbelgeUsers:  # The target object of the parser
     def __init__(self):
         self.tax_id = ""
         self.is_tax_data = False
-        self.name = ""
-        self.is_name = False
+        self.title = ""
+        self.is_title = False
         self.activity_count = 0
         self.is_einvoice_document = False
         self.is_edespatchadvice_document = False
@@ -15,7 +15,7 @@ class EbelgeUsers:  # The target object of the parser
         if tag == "Identifier":
             self.is_tax_data = True
         elif tag == "Title":
-            self.is_name = True
+            self.is_title = True
         elif tag == "Document":
             self.activity_count = 0
             if attrib["type"] == "Invoice":
@@ -29,7 +29,7 @@ class EbelgeUsers:  # The target object of the parser
         if tag == "Identifier":
             self.is_tax_data = False
         elif tag == "Title":
-            self.is_name = False
+            self.is_title = False
         elif tag == "CreationTime":
             self.activity_count += 1
         elif tag == "DeletionTime":
@@ -45,7 +45,7 @@ class EbelgeUsers:  # The target object of the parser
                 [
                     ("is_efatura_user", self.is_efatura_user),
                     ("is_eirsaliye_user", self.is_eirsaliye_user),
-                    ("name", self.name)
+                    ("company_title", self.title)
                 ]
             )
             self.setup()
@@ -53,8 +53,8 @@ class EbelgeUsers:  # The target object of the parser
     def data(self, data):
         if self.is_tax_data:
             self.tax_id = data
-        elif self.is_name:
-            self.name = data
+        elif self.is_title:
+            self.title = data
 
     def close(self):  # Called when all data has been parsed.
         return self.return_data
@@ -62,8 +62,8 @@ class EbelgeUsers:  # The target object of the parser
     def setup(self):
         self.tax_id = ""
         self.is_tax_data = False  #
-        self.name = ""
-        self.is_name = False
+        self.title = ""
+        self.is_title = False
         self.activity_count = 0  # use when document is of ebelge type to determine if active
         self.is_einvoice_document = False
         self.is_edespatchadvice_document = False
---
+++ trebelge/trebelge/doctype/ubl_tr_user_list/test_ubl_tr_user_list.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Framras AS-Izmir and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestUBLTRUserList(unittest.TestCase):
+	pass
